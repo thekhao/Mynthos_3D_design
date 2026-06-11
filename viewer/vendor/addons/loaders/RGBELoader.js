1d118ea383773feb27da5b9843357f468a4e5038
Albin Legrand <101952112+thekhao@users.noreply.github.com>
2026-06-11 18:15:21 +0200

    vendor: three.js addons (OrbitControls, RGBELoader, HDRLoader, BufferGeometryUtils)

---
+++ viewer/vendor/addons/loaders/RGBELoader.js
@@ -0,0 +1,16 @@
+import { HDRLoader } from './HDRLoader.js';
+
+// @deprecated, r180
+
+class RGBELoader extends HDRLoader {
+
+	constructor( manager ) {
+
+		console.warn( 'RGBELoader has been deprecated. Please use HDRLoader instead.' );
+		super( manager );
+
+	}
+
+}
+
+export { RGBELoader };
